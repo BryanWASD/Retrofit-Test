3d3bf1808b151d9319f2dcdf3c92edb28c3a6998
brian.zaz.205.gh <brian.zaz.205.gh@gmail.com>
2024-03-20 18:20:31 +0000

    first commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/models/post_model.dart';
+import 'package:myapp/services/api_service.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF020202),
+        title: const Text('Test', style: TextStyle(color: Colors.white
+        ),
+      ),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  FutureBuilder _body(){
+    final apiService = ApiService(Dio(BaseOptions(contentType: "application/json")));
+    return FutureBuilder(
+      future: apiService.getPosts(), 
+      builder: (
+        (context, snapshot){
+      if(snapshot.connectionState == ConnectionState.done){
+        final List<PostModel> posts = snapshot.data!;
+        return _post(posts);
+      } else{
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    } 
+    ));
+  }
+
+  Widget _post(List<PostModel> posts) {
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder:(context, index) {
+        return Container(
+        margin: const EdgeInsets.all(16), 
+        padding: const EdgeInsets.all(16), 
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black38,width: 1),
+        ),
+        child: Column(
+          children: [
+            Text(posts[index].title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Text(
+              posts[index].body
+            ),
+          ],
+        ),
+      );
+      },
+    );
+  }
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:myapp/models/post_model.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+part 'api_service.g.dart';
+
+
+@RestApi(baseUrl: "https://jsonplaceholder.typicode.com/")
+abstract class ApiService{
+  factory ApiService(Dio dio) = _ApiService;
+
+  @GET('posts')
+  Future<List<PostModel>> getPosts();
+  
+}
